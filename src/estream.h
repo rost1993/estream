24823042b7d3e85a5f0e737ca063101c30940cf3
root <root@debian.Home>
2015-03-20 22:37:34 +0300

    estream

---
+++ src/estream.h
@@ -0,0 +1,13 @@
+#define ESTREAM_H
+#ifndef ESTREAM_H
+
+#include "salsa.h"
+#include "rabbit.h"
+#include "hc128.h"
+#include "sosemanuk.h"
+#include "grain.h"
+#include "mickey.h"
+#include "trivium.h"
+#include "macro.h"
+
+#endif
