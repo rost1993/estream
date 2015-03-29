5da703a0f85a47faac16cdecad67febaaff6400d
root <root@debian.Home>
2015-03-29 10:23:06 +0300

    lol

--- test_src/test.c
+++ test_src/test.c
@@ -14,12 +14,16 @@ universe()
 int
 main(void)
 {
-	int (*p)(void) = universe;
+	int (*p)(void);
 	void *src;
 
-	if((src = mmap(0, sizeof(universe), PROT_EXEC|PROT_WRITE, MAP_SHARED, p(), 0)) == MAP_FAILED) {
+	p = universe;
+	
+	if((src = mmap(0, sizeof(p()), PROT_EXEC|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0)) == MAP_FAILED) {
 		printf("Error!!!!\n");
 	}
+
+	memcpy(src, p, sizeof(p()));
 
 	return 0;
 }
